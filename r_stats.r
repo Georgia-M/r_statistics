af2eed6e6e6eef32b3934cb4b55549c2212ac8cc
Georgia-M <58853622+Georgia-M@users.noreply.github.com>
2023-06-29 10:53:36 +0200

    Update r_stats.r

--- r_stats.r
+++ r_stats.r
@@ -1,5 +1,6 @@
 library("readtext")
 library(reshape2)
+library(ggplot2)
 
 
 file <- "C:/Users/gasto/OneDrive/Desktop/test.txt"
@@ -21,5 +22,29 @@ B <- data$B[!(data$B %in% out_index)]
 
 data <- stack(list(A = A, B = B))
 data <- na.omit(data)
+
+
+ggplot(don, aes(y=E, x=Concentration,colour=Concentration,fill=Concentration))+
+  geom_boxplot(alpha=0.5, outlier.alpha=0)+
+  geom_jitter(width=0.25)+
+  theme_classic()
+
+
+
+#perform Kolmogorov-Smirnov test (normality test, p<0.05 then data not normal)
+ks.test(A, B)
+
+#Shapiro–Wilk Test for normality (p<0.05 then not normal)
+shapiro.test(A)
+shapiro.test(B)
+
+#t-test
+t.test(A, B, alternative = "two.sided", var.equal = FALSE)
+
+#Wilcoxon test
+wilcox.test(A, B, alternative = "two.sided")
+
+
+
 
 
